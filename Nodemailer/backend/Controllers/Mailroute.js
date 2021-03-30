a891ffbcb67a6c8bd4f0b7ae7b7a9dcf383f5448
alfred-richards-au9 <67587493+alfred-richards-au9@users.noreply.github.com>
2021-03-30 11:22:34 +0530

    .

--- Nodemailer/backend/Controllers/Mailroute.js
+++ Nodemailer/backend/Controllers/Mailroute.js
@@ -1,34 +1,52 @@
-const express=require('express')
-const mailroute = express.Router()
-const nodemailer = require('nodemailer')
+const express = require("express");
+const mailroute = express.Router();
+const nodemailer = require("nodemailer");
+let otp = "";
 const transporter = nodemailer.createTransport({
-    service:'gmail',
-    auth:{
-        user:'benjimenrichards335@gmail.com',
-        pass:'Benjimen#123'
+  service: "gmail",
+  auth: {
+    user: "benjimenrichards335@gmail.com",
+    pass: "Benjimen#123",
+  },
+});
+
+mailroute.post("/", (req, res) => {
+  otp = Math.floor(Math.random() * 9999) + 1;
+  let mailoptions = {
+    from: "benjimenrichards335@gmail.com",
+    to: "richardsbenjialfred@gmail.com",
+    subject: "Testing nodemailer",
+    text: `Hello benjimen ur otp is ${otp}`,
+  };
+  transporter.sendMail(mailoptions, (err, data) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(`${otp}`);
     }
-})
+  });
+});
 
-let mailoptions = {
-    from :"benjimenrichards335@gmail.com",
-    to:"richardsbenjialfred@gmail.com",
-    subject:"Testing nodemailer",
-    text:"hello Benjimen"
-}
-
-transporter.sendMail(mailoptions,(err,data)=>{
-    if(err)
+mailroute.post('/auth',(req,res)=>
+{
+    const verify=parseInt(req.headers['otp'])
+    if(otp === verify)
     {
-        console.log(err)
+        res.send({
+            s:"success",
+            otp,
+            verify
+        })
     }
     else
     {
-        console.log("Email send")
+        res.send({
+            s:"invalid",
+            otp,
+            verify
+        })
     }
 })
 
-mailroute.get('/',(req,res)=>
-{
-    res.send("mail route")
-})
-module.exports=mailroute
+
+module.exports = mailroute;
